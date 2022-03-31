23b7a840d759ef66e392c5c8c17db5d9aabf9187
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-03-31 21:22:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.use(express.static(path.join(__dirname, 'public'), {index: '_'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+app.set('view-engine', 'html');
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
