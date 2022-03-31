61dea8c4a88af4f6890cefe39a7415d76f59047a
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-03-31 21:39:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, '/public'));
 //app.set('view engine', 'html');
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
