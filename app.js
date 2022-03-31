d902f9719ecfe70163ebf6484c624427c13389c4
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-03-31 21:42:09 +0800

    Updated

--- app.js
+++ app.js
@@ -15,15 +15,14 @@ var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
 app.use(bodyParser.raw({type: 'application/jwt'}));
 app.use(express.static(path.join(__dirname, 'public'), {index: '_'}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-//app.set('views', path.join(__dirname, '/public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/public'));
 //app.set('view engine', 'html');
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
