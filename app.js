bf285e5e5605e5ceb0ea222fbb5df04f1490f5f7
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-03-31 21:38:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ var app = express();
 // Configure Express
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
-app.use(express.static(path.join(__dirname, '/public'), {index: '_'}));
+app.use(express.static(path.join(__dirname, 'public'), {index: '_'}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public'));
 //app.set('view engine', 'html');
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
